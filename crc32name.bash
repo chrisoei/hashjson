94fe38e42af61af3b79fad4c4c045885979b4b6e
U-V6110US\software <c20080619-github@yahoo.com>
2008-07-25 11:02:37 -0700

    Bug fix in crc32name.bash for multiple periods in filename.

--- crc32name.bash
+++ crc32name.bash
@@ -3,7 +3,13 @@
 x="$1"
 crc32=`cko-multidigest "$x" | grep 'CRC32:' | awk '{print $2}'|tr '[a-z]' '[A-Z]'`
 
+if [ ! -z "$crc32" ]
+then
+  y="${x%.*} [$crc32].${x##*.}"
+  echo $y
+  mv "$x" "$y"
+else
+  echo "Unable to crc32 $x!"
+fi
 
-y="${x%.*} [$crc32].${x#*.}"
-mv "$x" "$y"
 
