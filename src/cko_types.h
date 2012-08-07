61afbb78884cfefd5c72bd99d5fef10a8b065b7a
Chris Oei <chris@oei.me>
2012-08-06 19:33:24 -0700

    Refactor to use cko_stomach in both ckoei_multidigest and hashjson

--- src/cko_types.h
+++ src/cko_types.h
@@ -15,4 +15,36 @@ typedef long long cko_s64;
 
 int cko_types_test();
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
+#include <zlib.h>
+
+typedef struct {
+  char* filename;
+  cko_u32 chunksize;
+  MD5_CTX md5_ctx;
+  SHA_CTX sha1_ctx;
+  SHA256_CTX sha256_ctx;
+  SHA512_CTX sha512_ctx;
+  RIPEMD160_CTX ripemd160_ctx;
+  cko_u32 adler32;
+  cko_u32 crc32;
+  cko_u64 size;
+  char hex_adler32[8+1];
+  char hex_crc32[8+1];
+  char hex_md5[32+1];
+  char hex_sha1[40+1];
+  char hex_sha256[64+1];
+  char hex_sha512[128+1];
+  char hex_ripemd160[40+1];
+} cko_multidigest_t,*cko_multidigest_ptr;
+
+void cko_multidigest_init(cko_multidigest_ptr x);
+void cko_multidigest_update(cko_multidigest_ptr x, unsigned char* s,cko_u32 l);
+void cko_multidigest_final(cko_multidigest_ptr x);
+
 #endif // CKO_TYPES
