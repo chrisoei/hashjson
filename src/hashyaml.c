21a981d14cb1e7dd3370518b182934733f4f1f32
Chris Oei <zlkm208-github1@yahoo.com>
2013-01-04 16:11:03 -0800

    Add hashyaml executable

---
+++ src/hashyaml.c
@@ -0,0 +1,28 @@
+#include <cko_types.h>
+
+void cko_multidigest_file(cko_multidigest_ptr ctx);
+
+void cko_multidigest_print_yaml(cko_multidigest_ptr x) {
+  printf("adler32: '%s'\n",x->hex_adler32);
+  printf("crc32: '%s'\n",x->hex_crc32);
+  printf("md5: '%s'\n",x->hex_md5);
+  printf("sha1: '%s'\n",x->hex_sha1);
+  printf("sha256: '%s'\n",x->hex_sha256);
+  printf("sha512: '%s'\n",x->hex_sha512);
+  printf("ripemd160: '%s'\n",x->hex_ripemd160);
+  printf("size: '%llu'\n",x->size);
+  printf("version: 'hashyaml-%s'\n", HASHJSON_VERSION);
+}
+
+int main(int argc,char* argv[]) {
+  cko_types_test();
+  cko_multidigest_t m;
+
+  cko_multidigest_init(&m);
+  if (argc == 2) {
+    m.filename = argv[1];
+  }
+  cko_multidigest_file(&m);
+  cko_multidigest_print_yaml(&m);
+  return 0;
+}
